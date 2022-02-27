f6f1aaccf2c218d920d54f840d5ee827ebf427bc
Peter Solymos <psolymos@gmail.com>
2022-02-27 00:34:11 -0700

    Update processes
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

---
+++ scripts/01-process-qs-files.R
@@ -0,0 +1,85 @@
+## Process large bird result files
+## chunk results into series of nested directories
+## writing parquet files instead of qs
+## Input file: ALFL_rastersSummaryTable.qs
+
+# Classes ‘data.table’ and 'data.frame':	381193620 obs. of  12 variables:
+#  $ LandR_fS_V4      : num  0.181 0.18 0.196 0.253 0.184 ...
+#  $ location         : num  2 2 2 2 2 2 2 2 2 NA ...
+#  $ pixelID          : int  38546 38547 38548 38549 41470 41471 41472 41473 41474 41475 ...
+#  $ Run              : chr  "run1" "run1" "run1" "run1" ...
+#  $ Year             : num  2011 2011 2011 2011 2011 ...
+#  $ LandR_fS_V6a     : num  0.143 0.143 0.144 0.171 0.143 ...
+#  $ LandR.CS_fS_V4   : num  0.181 0.18 0.196 0.253 0.184 ...
+#  $ LandR.CS_fS_V6a  : num  0.143 0.143 0.144 0.171 0.143 ...
+#  $ LandR_SCFM_V4    : num  0.171 0.171 0.196 0.238 0.171 ...
+#  $ LandR_SCFM_V6a   : num  0.144 0.144 0.144 0.171 0.144 ...
+#  $ LandR.CS_SCFM_V4 : num  0.171 0.171 0.196 0.238 0.171 ...
+#  $ LandR.CS_SCFM_V6a: num  0.144 0.144 0.144 0.171 0.144 ...
+
+
+## Output: <species>/<year>/<run>/part-0.parquet
+
+# FileSystemDataset with 1 Parquet file
+# land_r_f_s_v4: double
+# location: double
+# pixel_id: int32
+# land_r_f_s_v6a: double
+# land_r_cs_f_s_v4: double
+# land_r_cs_f_s_v6a: double
+# land_r_scfm_v4: double
+# land_r_scfm_v6a: double
+# land_r_cs_scfm_v4: double
+# land_r_cs_scfm_v6a: double
+
+library(qs)
+library(arrow)
+library(dplyr)
+library(janitor)
+
+finp <- list.files("qsfiles", full.names = TRUE, pattern = "gz")
+
+for (i in seq_along(finp)) {
+  message(finp[i])
+  fout <- paste0("arrow/", gsub("qsfiles/|.qs", "", finp[i]))
+  if (!file.exists(fout)) {
+    d <- qread(finp[i])
+
+    d <- d %>%
+      clean_names() %>%
+      as_tibble()
+
+    Y <- unique(d$year)
+
+    j <- 1
+
+    for (y in Y) {
+      message(y)
+      if (j == 1) {
+        d <- d %>%
+          filter(year == y) %>%
+          group_by(year, run)
+
+        gc()
+
+        d %>%
+          write_dataset(fout)
+
+        rm(d); gc()
+
+        j <- j + 1
+      } else {
+        d <- qread(finp[i]) %>%
+          clean_names() %>%
+          as_tibble() %>%
+          filter(year == y) %>%
+          group_by(year, run)
+
+        d %>%
+          write_dataset(fout)
+
+        rm(d); gc()
+      }
+    }
+  }
+}
--- scripts/gettingSummarizedDataToRaster.R
+++ scripts/gettingSummarizedDataToRaster.R
@@ -1,14 +1,4 @@
-# SCENARIOS
-#  LandR: non-climate-sensitive version of forest growth model
-#  LandR.CS: climate-sensitive version of forest growth model
-#  SCFM: non-climate-sensitive version of wildfire model
-#  fS: climate-sensitive version of wildfire model
-#  V4: non-climate-sensitive version of bird model
-#  V6a: climate-sensitive version of bird model
-#    i.e., Fully non-climate sensitive model: LandR_SCFM_V4
-#    i.e., Fully climate sensituive model: LandR.CS_fS_V6a
-# RUN: each Run (i.e., "run1") is a replicate for stochastic processes# YEAR: each Year of the simulations
-# pixelID: Index to match the raster
+## qs data processing script provided by T. Micheletti
 
 # Load/install 
 libraries:if (!require("Require")) {
@@ -41,3 +31,4 @@ tbCS <- merge(data.table(pixelID = 1:ncell(templateRas)), tbCS, all.x = TRUE)
 # 5. Set the values to the raster:
 csRas <- setValues(x = templateRas, values = tbCS[["LandR.CS_fS_V6a"]])
 plot(csRas)
+
