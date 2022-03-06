bbf1e3e920ada55b6b7aca45963977d7c5a07e04
Peter Solymos <psolymos@gmail.com>
2022-03-06 11:30:33 -0700

    Add code to combine regions
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- data/04-study-area-and-regions.R
+++ data/04-study-area-and-regions.R
@@ -68,6 +68,8 @@ x <- st_transform(x, st_crs(r))
 x <- st_intersection(x, z)
 x <- x[,setdiff(names(x), names(z))]
 x[["Shape_Length"]] <- x[["Shape_Area.1"]] <- NULL
+x <- x[x$LOC_E == "Northwest Territories",]
+any(duplicated(x$NAME_E))
 
 o <- "protected-areas-in-nwt-bcr6"
 png(sprintf("data/regions/%s.png", o))
@@ -93,3 +95,35 @@ png(sprintf("data/regions/%s.png", o))
 plot(xx$Shape, border=as.factor(xx$layer))
 dev.off()
 st_write(xx, sprintf("data/regions/%s.gpkg", o))
+
+## combine regions into a single sf object
+
+library(sf)
+library(dplyr)
+
+p1 <- st_read("data/regions/ecodistricsts-in-nwt-bcr6.gpkg")
+p1 <- p1 |> group_by(ECOREGION) |> summarize()
+p1$classification <- "Ecoregions"
+p1$region <- as.character(p1$ECOREGION)
+p1$ECOREGION <- NULL
+p1$area <- st_area(p1)
+
+p2 <- st_read("data/regions/protected-areas-in-nwt-bcr6.gpkg")
+p2$classification <- "Protected Areas"
+p2$region <- p2$NAME_E
+p2$area <- st_area(p2)
+p2 <- p2[names(p1)]
+
+p3 <- st_read("data/regions/iwa-in-nwt-bcr6.gpkg")
+p3$layer <- gsub("BIO_ENR_WFE_", "", p3$layer)
+p3$layer <- gsub("_", " ", p3$layer)
+p3$classification <- p3$layer
+p3$region <- p3$IWA
+p3$area <- st_area(p3)
+p3 <- p3[names(p1)]
+
+pp <- rbind(p1, p2, p3)
+
+st_write(pp, "data/regions/regions.gpkg")
+saveRDS(pp, "data/regions/regions.rds")
+
