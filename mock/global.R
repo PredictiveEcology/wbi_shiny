a6cb7e81062b9fae489d2d16f6f82a0bdc032ae2
Peter Solymos <psolymos@gmail.com>
2022-03-11 10:44:26 -0700

    Add sf
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- mock/global.R
+++ mock/global.R
@@ -2,6 +2,7 @@ library(shiny)
 #library(shinythemes)
 library(leaflet)
 library(reactable)
+library(sf)
 
 BASEURL <- "https://wbi-nwt.analythium.app/api/v1/public"
 ELEMENTS <- read.csv("../data/element-lookup.csv")
@@ -19,5 +20,12 @@ x <- data.frame(
     ELEMENTS[match(LINKS$element, rownames(ELEMENTS)), -1])
 x <- x[x$resolution != "tiles",c("group", "species_code", "common_name", "scientific_name", 
     "scenario", "period", "resolution", "path")]
+
+a <- st_read("data/regions/nwt-bcr6.gpkg")
+p <- st_read("data/regions/regions.gpkg")
+a$classification <- "StudyArea"
+a$region <- "NWT"
+a$area <- st_area(a)
+p <- rbind(a[,colnames(p)], p)
 
 source("functions.R")
