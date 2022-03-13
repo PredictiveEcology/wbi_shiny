a83955a73b7117eb00f0bbf0af0e15d802bbe99a
Peter Solymos <psolymos@gmail.com>
2022-03-12 19:30:12 -0700

    More experiments
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- mock/index.R
+++ mock/index.R
@@ -110,6 +110,8 @@ leaflet() %>%
   ## there is also an unexported addCOG()
 
 library(shiny)
+library(leaflet)
+library(leafem)
 
 ## note Leaflet is having problem with CORS requests
 shinyApp(
@@ -121,13 +123,13 @@ shinyApp(
       leaflet() %>%
         addProviderTiles("Esri.WorldImagery") %>%
         addGeotiff(
-          url = "https://peter.solymos.org/testapi/amro1k.tif",
+          url = "https://peter.solymos.org/testapi/amro1k-stars.tif",
           project = FALSE,
           opacity = 0.8,
           colorOptions = colorOptions(
              palette = hcl.colors(50, palette = "inferno"), 
              domain = c(0, 0.62),
-             na.color = "#00000000"))
+             na.color = "transparent"))
     })
   }
 )
@@ -140,3 +142,112 @@ leaflet() %>% addTiles() %>% addMeasure()
 
 ## Opacity slider
 # https://cran.r-project.org/web/packages/leaflet.opacity/vignettes/leaflet-opacity.html
+
+library(raster)
+r <- raster("https://peter.solymos.org/testapi/amro1k.tif")
+NAvalue(r)
+NAvalue(r) <- -9999
+NAvalue(r)
+plot(r)
+#writeRaster(r, "amro1k9999.tif")
+#r <- raster("https://peter.solymos.org/testapi/amro1k9999.tif")
+#NAvalue(r)
+
+library(stars)
+s <- st_as_stars(r)
+write_stars(s, "amro1k-stars.tif")
+
+## Make an app where color palettes can be picked
+
+plot_pal <- function(pal, pch=15) {
+  plot(seq_along(pal), rep(0, length(pal)), 
+    col=pal, pch=pch, cex=5, axes=FALSE, ann=FALSE)
+}
+
+get_pal <- function(n=50, type=c("viridis", "rdylbu", "bam")) {
+  switch(match.arg(type),
+    "viridis" = viridis::viridis_pal(option = "D")(n),
+    "rdylbu" = grDevices::hcl.colors(n, "RdYlBu", rev = TRUE),
+    "bam" = grDevices::colorRampPalette(
+        c("#FFFACD", "lemonchiffon","#FFF68F", "khaki1","#ADFF2F", 
+        "greenyellow", "#00CD00", "green3", "#48D1CC", "mediumturquoise", 
+        "#007FFF", "blue"), space="Lab", bias=0.5)(n))
+}
+
+# get_pal(50, "viridis") |> plot_pal()
+# get_pal(50, "rdylbu") |> plot_pal()
+# get_pal(50, "bam") |> plot_pal()
+
+base <- function(type) {
+  map_attr = "© <a href='https://www.esri.com/en-us/home'>ESRI</a> © <a href='https://www.google.com/maps/'>Google</a> © <a href='https://ebird.org/science/status-and-trends'>eBird / Cornell Lab of Ornithology</a> © <a href='https://www.gatesdupont.com/'>Gates Dupont</a>"
+  leaflet() %>%
+  addTiles(urlTemplate = "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga",
+          group  =  "Google") %>%
+  addProviderTiles("CartoDB.Positron", group = "CartoDB") %>%
+  addProviderTiles("OpenStreetMap", group = "Open Street Map") %>%
+  addProviderTiles('Esri.WorldImagery', group = "ESRI") %>%
+  addTiles(urlTemplate = "", attribution = map_attr) %>%
+  addLayersControl(
+    baseGroups = c("ESRI", "Open Street Map", "CartoDB", "Google"),
+    options = layersControlOptions(collapsed = FALSE)) %>%
+  setView(-120, 65, 5)
+
+}
+lfun <- function(map, type="viridis", opacity=0.8) {
+  addGeotiff(map,
+      url = "https://peter.solymos.org/testapi/amro1k-stars.tif",
+      project = FALSE,
+      opacity = opacity,
+      autozoom = FALSE,
+      layerId = "raster",
+      colorOptions = colorOptions(
+        palette = get_pal(50, type), 
+        domain = c(0, 0.62),
+        na.color = "transparent")) %>%
+    addLegend(pal = colorNumeric(palette=get_pal(50, type), domain = c(0, 0.62)), 
+      values = c(0, 0.62),
+      title = "Abundance")
+}
+
+library(shiny)
+library(leaflet)
+library(leafem)
+
+shinyApp(
+  ui=fluidPage(
+    sidebarLayout(
+      sidebarPanel(
+        h1("Color Palette Test"),
+        selectInput("type", "Color palette:", c("viridis", "rdylbu", "bam")),
+        sliderInput("opacity", label = "Opacity:", 
+                        min = 0, value = 0.8, max = 1),
+        p("Please try the 3 palettes and change the base map.",
+        "Provide feedback by filling out the form at the bottom of the page. Thanks!")
+      ),
+       mainPanel(
+        leafletOutput("map", width = "100%", height = 600),
+        HTML(Form)
+       )
+    )
+  ),
+  server=function(input, output) {
+    output$map <- renderLeaflet({
+      base() %>%
+        lfun(input$type, input$opacity)
+    })
+  }
+)
+
+base() %>% lfun()
+
+Form <- '<div id="my-reform"></div>
+
+<script>window.Reform=window.Reform||function(){(Reform.q=Reform.q||[]).push(arguments)};</script>
+<script id="reform-script" async src="https://embed.reform.app/v1/embed.js"></script>
+<script>
+    Reform("init", {
+        url: "https://forms.reform.app/analythium/wbi-palette-test/lulyw8",
+        target: "#my-reform",
+        background: "default",
+    })
+</script>'
