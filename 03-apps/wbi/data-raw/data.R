215907c72bb4858186df9754a3cd4f9beb47daf5
Michael Thomas <mthomas@ketchbrookanalytics.com>
2023-05-17 00:14:14 -0400

    Updated `ELEMENT_NAMES` & `MAPSTATS` datasets

--- 03-apps/wbi/data-raw/data.R
+++ 03-apps/wbi/data-raw/data.R
@@ -1,7 +1,17 @@
 ## code to prepare `data` dataset goes here
 
+# ELEMENTS ----
 ELEMENTS <- read.csv("data-raw/element-lookup.csv")
 rownames(ELEMENTS) <- paste0(ELEMENTS$group, "-", tolower(ELEMENTS$species_code))
+
+# ELEMENT_NAMES ----
+ELEMENT_NAMES <- tolower(ELEMENTS$species_code)
+names(ELEMENT_NAMES) <- ELEMENTS$common_name
+
+ELEMENT_NAMES <- list(
+  bird = ELEMENT_NAMES[ELEMENTS$group == "bird"],
+  tree = ELEMENT_NAMES[ELEMENTS$group == "tree"]
+)
 
 # TODO // Peter adding 2 more columns, one is the max value, and the second is 
 # the max index value for the hex code to drive the color scaling... need to add
@@ -10,21 +20,26 @@ LINKS <- jsonlite::fromJSON(
   "https://wbi-nwt.analythium.app/api/v1/public/wbi-nwt/elements/index.json"
 )
 
-MAPSTATS <- read.csv("data-raw/element-stats.csv")
-MAPSTATS <- MAPSTATS[MAPSTATS$resolution == "1000m", 
-  c("element_name", "year", "scenario", "max", "pal_max")]
+# MAPSTATS ----
+# Retrieve .rds file containing MAPSTATS data from API
+MAPSTATS <- paste0(
+  get_golem_config("app_baseurl"),
+  "api/v1/public/wbi/maps-lonlat-with-stats.rds"
+) |> 
+  url() |> 
+  readRDS()
 
+# Keep only the columns needed for legend gradients
+MAPSTATS$max <- MAPSTATS$q1
+MAPSTATS <- MAPSTATS[, c("region", "element", "scenario", "period", "max")]
+
+# SCENARIOS ----
 SCENARIOS <- list(
-  "LandR SCFM V4" = "landr-scfm-v4",
-  "LandR.CS FS V6a" = "landrcs-fs-v6a"
+  "CNRM-ESM2-1 SSP370" = "cnrm-esm2-1-ssp370", 
+  "CanESM5 SSP370" = "canesm5-ssp370", 
+  "CNRM-ESM2-1 SSP585" = "cnrm-esm2-1-ssp585", 
+  "CanESM5 SSP585" = "canesm5-ssp585"
 )
-
-ELEMENT_NAMES <- paste0(ELEMENTS$group, "-", tolower(ELEMENTS$species_code))
-names(ELEMENT_NAMES) <- ELEMENTS$common_name
-
-ELEMENT_NAMES <- list(
-  bird = ELEMENT_NAMES[ELEMENTS$group == "bird"],
-  tree = ELEMENT_NAMES[ELEMENTS$group == "tree"])
 
 
 MAIN <- data.frame(
