eef8396ab3355296bd2fc6297b83802b059d4beb
Michael Thomas <mthomas@ketchbrookanalytics.com>
2023-08-15 11:59:36 -0400

    Removed unused code

--- 03-apps/wbi/R/mod_map.R
+++ 03-apps/wbi/R/mod_map.R
@@ -69,64 +69,6 @@ mod_map_ui <- function(id){
           selected = ELEMENTS[ELEMENTS$species_code == ELEMENT_NAMES$bird[[1]], "year_start"]
         ),
         
-        # build_accordion(
-        #   id = "map",
-        #   header = "You are now viewing:",
-        #   content = shiny::tags$ul(
-        #     
-        #     shiny::tags$li(
-        #       shiny::span(
-        #         "Region: ",
-        #         shiny::textOutput(
-        #           outputId = ns("region_text"),
-        #           inline = TRUE
-        #         ) |> shiny::tags$em()
-        #       )
-        #     ),
-        #     
-        #     shiny::tags$li(
-        #       shiny::span(
-        #         "Species Group: ", 
-        #         shiny::textOutput(
-        #           outputId = ns("species_group_text"),
-        #           inline = TRUE
-        #         ) |> shiny::tags$em()
-        #       )
-        #     ),
-        #     
-        #     shiny::tags$li(
-        #       shiny::span(
-        #         "Species Name: ", 
-        #         shiny::textOutput(
-        #           outputId = ns("species_name_text"),
-        #           inline = TRUE
-        #         ) |> shiny::tags$em()
-        #       )
-        #     ),
-        #     
-        #     shiny::tags$li(
-        #       shiny::span(
-        #         "Scenario: ", 
-        #         shiny::textOutput(
-        #           outputId = ns("scenario_text"),
-        #           inline = TRUE
-        #         ) |> shiny::tags$em()
-        #       )
-        #     ),
-        #     
-        #     shiny::tags$li(
-        #       shiny::span(
-        #         "Time Period: ", 
-        #         shiny::textOutput(
-        #           outputId = ns("period_text"),
-        #           inline = TRUE
-        #         ) |> shiny::tags$em()
-        #       )
-        #     )
-        #     
-        #   )
-        # ),
-        
         shiny::actionButton(
           inputId = ns("edit_map_settings"),
           label = "Change Preferences",
@@ -148,23 +90,15 @@ mod_map_server <- function(id, elements){
     ns <- session$ns
     
     ## Set Initial Filter Selections ----
-    # Create a `reactiveValues` list to hold current selections for each filter;
-    # This will help make user choices redundant when re-launching the modal;
+    # Create a `reactiveValues` list to hold current selections for each filter
+    # in the modal; this will help make user choices redundant when re-launching
+    # the modal.
     # Start by setting some defaults that will appear the first time the modal
     # is launched
     current_selections <- shiny::reactiveValues(
       region = REGIONS[[1]],
-      # element_type = "bird",
-      # element_choices = ELEMENT_NAMES$bird,
-      # element = ELEMENT_NAMES$bird[[1]],
-      # scenario = SCENARIOS[[1]],
-      # period = ELEMENTS[ELEMENTS$species_code == ELEMENT_NAMES$bird[[1]], "year_start"],
-      # period_choices = get_period_choices(
-      #   ELEMENTS[ELEMENTS$species_code == ELEMENT_NAMES$bird[[1]], ]
-      # ),
       palette = "spectral",
-      opacity = 0.8#,
-      # element_display = ELEMENT_NAMES$bird[[1]]
+      opacity = 0.8
     )
     
     ## Modal ----
@@ -175,82 +109,30 @@ mod_map_server <- function(id, elements){
         
         title = "Set Map Preferences",
         
-        
-        shiny::fluidRow(
-          shiny::column(
-            width = 12, 
-            
-            selectInput(
-              inputId = ns("map_region"),
-              label = "Region:", 
-              choices = REGIONS,
-              selected = current_selections$region
-            ),
-            
-            # shinyWidgets::pickerInput(
-            #   inputId = ns("map_element"),
-            #   label = "Species Name:", 
-            #   choices = ELEMENT_NAMES, 
-            #   selected = current_selections$element, 
-            #   options = list(
-            #     `live-search` = TRUE
-            #     # style = "border-color: #999999;"
-            #     # style = paste0(
-            #     #   "background-color: white; ",
-            #     #   "border-color: #999999; ",
-            #     #   "font-family: 'Helvetica Neue' Helvetica; ",
-            #     #   "font-weight: 200;"
-            #     # )
-            #   )
-            # ),
-            
-            # selectInput(
-            #   inputId = ns("map_scenario"),
-            #   label = "Scenario:",
-            #   choices = SCENARIOS,
-            #   selected = current_selections$scenario
-            # ),
-            # 
-            # selectInput(
-            #   inputId = ns("map_period"), 
-            #   label = "Time Period:", 
-            #   choices = current_selections$period_choices,
-            #   selected = current_selections$period
-            # )
-            
-          )
-        ), 
-        
-        shiny::fluidRow(
-          shiny::column(
-            width = 12, 
-            
-            selectInput(
-              inputId = ns("map_palette"), 
-              label = "Color Palette:", 
-              choices = c(
-                "Spectral" = "spectral", 
-                "Viridis" = "viridis", 
-                "Red Yellow Blue" = "rdylbu", 
-                "BAM" = "bam"),
-              selected = current_selections$palette
-            )
-          )
+        selectInput(
+          inputId = ns("map_region"),
+          label = "Region:", 
+          choices = REGIONS,
+          selected = current_selections$region
         ),
         
-        shiny::fluidRow(
-          shiny::column(
-            width = 12,
-            
-            sliderInput(
-              inputId = ns("map_opacity"), 
-              label = "Opacity:", 
-              min = 0, 
-              max = 1, 
-              value = current_selections$opacity
-            )
-            
-          )
+        selectInput(
+          inputId = ns("map_palette"), 
+          label = "Color Palette:", 
+          choices = c(
+            "Spectral" = "spectral", 
+            "Viridis" = "viridis", 
+            "Red Yellow Blue" = "rdylbu", 
+            "BAM" = "bam"),
+          selected = current_selections$palette
+        ),
+        
+        sliderInput(
+          inputId = ns("map_opacity"), 
+          label = "Opacity:", 
+          min = 0, 
+          max = 1, 
+          value = current_selections$opacity
         ),
         
         footer = shiny::actionButton(
@@ -258,7 +140,7 @@ mod_map_server <- function(id, elements){
           label = "Apply"
         ),
         
-        size = "m"
+        size = "s"
         
       )
       
@@ -285,21 +167,8 @@ mod_map_server <- function(id, elements){
     shiny::observeEvent(input$close_modal, {
       
       current_selections$region <- input$map_region
-      # current_selections$element <- input$map_element
-      # current_selections$scenario <- input$map_scenario
-      # current_selections$period <- input$map_period
-      # current_selections$period_choices <- get_period_choices(
-      #   ELEMENTS[ELEMENTS$species_code == input$map_element, ]
-      # )
       current_selections$palette <- input$map_palette
       current_selections$opacity <- input$map_opacity
-      
-      # current_selections$element_type <- lookup_element_type_by_value(
-      #   x = ELEMENT_NAMES, 
-      #   value = input$map_element
-      # )
-      # current_selections$element_choices <- ELEMENT_NAMES[[current_selections$element_type]]
-      # current_selections$element_display <- current_selections$element
       
       shiny::removeModal(session = session)
       
@@ -354,54 +223,6 @@ mod_map_server <- function(id, elements){
         ")
       
     })
-    
-    # Summary Text ----
-    
-    # # Render "Region" selection text
-    # output$region_text <- shiny::renderText(
-    #   
-    #   lookup_element_name_by_value(
-    #     x = REGIONS,
-    #     value = current_selections$region
-    #   )
-    #   
-    # )
-    
-    # # Render "Species Group" selection text
-    # output$species_group_text <- shiny::renderText(
-    #   
-    #   current_selections$element_type |> 
-    #     tools::toTitleCase()
-    #   
-    # )
-    
-    # # Render "Species Name" selection text
-    # output$species_name_text <- shiny::renderText({
-    #   
-    #   shiny::req(
-    #     current_selections$element_type,
-    #     current_selections$element_display
-    #   )
-    #   
-    #   lookup_element_name_by_value(
-    #     x = ELEMENT_NAMES[[current_selections$element_type]],
-    #     value = current_selections$element_display
-    #   )
-    #   
-    # })
-    
-    # # Render "Scenario" selection text
-    # output$scenario_text <- shiny::renderText({
-    #   
-    #   lookup_element_name_by_value(
-    #     x = SCENARIOS,
-    #     value = current_selections$scenario
-    #   )
-    #   
-    # })
-    
-    # # Render "Period" selection text
-    # output$period_text <- shiny::renderText(current_selections$period)
     
   })
 }
